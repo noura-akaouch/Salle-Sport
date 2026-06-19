887460f87e1294e0d7fb54b29414bb87152fdfa5
Noura Akaouch <nouraakaouch2@gmail.com>
2026-06-19 12:13:08 +0100

    deuxieme commit

--- src/util/Database.java
+++ src/util/Database.java
@@ -1,4 +1,33 @@
-package util;
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
+
+    private static final String URL      = "jdbc:mysql://localhost:3306/salle_sport_db";
+    private static final String USER     = "root";
+    private static final String PASSWORD = "";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                connection = null;
+            }
+        }
+    }
 }
